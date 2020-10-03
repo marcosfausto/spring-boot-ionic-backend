d7573a9b785a6e8c6140038661ba88af9f66c6b6
Marcos Fausto <marcos_fausto@yahoo.com.br>
2020-10-03 16:20:01 +0100

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/marcosfausto/cursomc/domain/ItemPedido.java
+++ src/main/java/com/marcosfausto/cursomc/domain/ItemPedido.java
@@ -5,11 +5,14 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@EmbeddedId
+	@JsonIgnore
 	private ItemPedidoPK id = new ItemPedidoPK();
 	private Double desconto;
 	private Integer quantidade;
@@ -26,6 +29,7 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return this.id.getPedido();
 	}
@@ -34,6 +38,7 @@ public class ItemPedido implements Serializable {
 		return this.id.getProduto();
 	}
 
+	@JsonIgnore
 	public ItemPedidoPK getId() {
 		return id;
 	}
--- src/main/java/com/marcosfausto/cursomc/domain/Pagamento.java
+++ src/main/java/com/marcosfausto/cursomc/domain/Pagamento.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.marcosfausto.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -23,6 +24,7 @@ public abstract class  Pagamento implements Serializable {
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
+	@JsonBackReference
 	private Pedido pedido;
 	
 	public Pagamento() {
