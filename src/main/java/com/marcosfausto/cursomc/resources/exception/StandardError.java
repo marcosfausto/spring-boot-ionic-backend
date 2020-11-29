81de46ff7572b5c10f0fa5af8bf46b87c92a9fcf
Marcos Fausto <marcos_fausto@yahoo.com.br>
2020-11-29 16:29:58 +0000

    PUT,DELETE E GET para Cliente

--- src/main/java/com/marcosfausto/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/marcosfausto/cursomc/resources/exception/StandardError.java
@@ -1,7 +1,10 @@
 package com.marcosfausto.cursomc.resources.exception;
 
-public class StandardError {
-	
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
