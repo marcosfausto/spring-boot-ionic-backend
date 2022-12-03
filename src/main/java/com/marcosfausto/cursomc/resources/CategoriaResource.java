7f38348fa68db73796d7b5cdc2b6783e889a6b1d
Marcos <marcos_fausto@yahoo.com.br>
2022-12-03 12:38:51 +0000

    Documentação com Swagger - Parte 2

--- src/main/java/com/marcosfausto/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcosfausto/cursomc/resources/CategoriaResource.java
@@ -1,26 +1,22 @@
 package com.marcosfausto.cursomc.resources;
 
-import java.net.URI; 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
+import com.marcosfausto.cursomc.domain.Categoria;
+import com.marcosfausto.cursomc.dto.CategoriaDTO;
+import com.marcosfausto.cursomc.services.CategoriaService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.marcosfausto.cursomc.domain.Categoria;
-import com.marcosfausto.cursomc.dto.CategoriaDTO;
-import com.marcosfausto.cursomc.services.CategoriaService;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -28,7 +24,8 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
-	
+
+	@ApiOperation(value="Busca por id")
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
@@ -36,6 +33,7 @@ public class CategoriaResource {
 
 	}
 
+	@ApiOperation(value="Insere categoria")
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
@@ -46,6 +44,7 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 
+	@ApiOperation(value="Atualiza categoria")
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
@@ -55,21 +54,27 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@ApiOperation(value="Deleta uma categoria")
 	@PreAuthorize("hasAnyRole('ADMIN')")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = "Não é possível excluir uma categoria que possui produtos"),
+			@ApiResponse(code = 404, message = "Código inexistente") })
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		find(id);
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@ApiOperation(value="Retorna todas as categorias")
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List <CategoriaDTO>> findAll(){
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
+	@ApiOperation(value="Retorna todas as categorias com paginação")
 	@RequestMapping(value="/page", method=RequestMethod.GET)
 	public ResponseEntity<Page <CategoriaDTO>> findPage(
 			@RequestParam(value="page", defaultValue="0") Integer page,
